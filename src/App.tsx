f8e58663ef213446dafd13af394b718aff2db989
alkuzbl <84181803+alkuzbl@users.noreply.github.com>
2021-10-08 19:14:55 +0300

    Examination count

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Count} from "./components/Count";
+import style from "./components/Count.module.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //стейт для окна счетчика
+    let [numberCount, setNumberCount] = useState<number>(0)
+    //стейт для блокировки кнопок и стилизации
+    let [error, setError] = useState<boolean>(false)
+    //добавляем +1 при нажатии на клавишу
+    const onInk = () => {
+        setNumberCount(numberCount + 1)
+        //блокируем/разблокируем кнопку при достижении нужного числа
+        if (numberCount === 4) {
+            setError(true)
+        }
+    }
+    //сброс счетчика и установка начальных значений
+    const onReset = () => {
+        setNumberCount(0)
+        setError(false)
+    }
+    // стили кнопок и окна счетчика
+    let styleReset = !error ? style.counter__btnReset : style.error
+    let styleWindow = !error ? style.counter__window : style.errorWin
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Count numberCount={numberCount}
+                       onInk={onInk}
+                       onReset={onReset}
+                       styleWindow={styleWindow}
+                       error={error}
+                       styleReset={styleReset}
+                />
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -1,29 +1,18 @@
 import React, {useState} from "react";
-import {AlternativeSuperButton} from "./c2-SuperButton/AlternativeSuperButton";
+import {AlternativeSuperButton} from "./SuperButton/AlternativeSuperButton";
 import style from './Count.module.scss'
 
+type CountPropsType = {
+    numberCount: number
+    onInk: () => void
+    onReset: () => void
+    styleWindow: string
+    error: boolean
+    styleReset: string
+}
 
-export const Count = () => {
-    //стейт для окна счетчика
-    let [numberCount, setNumberCount] = useState<number>(0)
-    //стейт для блокировки кнопок и стилизации
-    let [error, setError] = useState(false)
-    //добавляем +1 при нажатии на клавишу
-    const onInk = () => {
-        setNumberCount(numberCount + 1)
-        //блокируем/разблокируем кнопку при достижении нужного числа
-        if (numberCount === 4) {
-            setError(true)
-        }
-    }
-    //сброс счетчика и установка начальных значений
-    const onReset = () => {
-        setNumberCount(0)
-        setError(false)
-    }
-    // стили кнопок и окна счетчика
-    let styleReset = !error ? style.counter__btnReset : style.error
-    let styleWindow = !error ? style.counter__window : style.errorWin
+export const Count: React.FC<CountPropsType> = ({numberCount, onInk, onReset, styleWindow, error, styleReset}) => {
+
     return (
         <div className={style.counter}>
             <div className={styleWindow}>
